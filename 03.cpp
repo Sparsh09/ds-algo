a37ed057d5b033e27ed5f19d7ebc2eb267e57a7e
Sparsh Mehta <mehtasparsh777@gmail.com>
2023-12-12 23:51:47 +0530

    Practice

---
+++ 03.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number " << endl;
+    if (cin >> n)
+    {
+        cout << "Printing" << n << endl;
+    }
+
+    if (cout << "Enter" << endl)
+    {
+        cout << "Enter a number\n";
+    }
+
+    char a = 'b';
+    int b = 97;
+    cout << a + b << endl;
+    cout << (float)(a + b) << endl;
+    float c = 3.3;
+    cout << c + a + b << endl;
+
+    float f = 2.7;
+    int p = 155;
+    cout << p - f << endl;
+}
